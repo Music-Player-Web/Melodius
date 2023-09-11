14fce9ff2568fd419d044533e330eac206ef69f4
fa6imalaali11 <fa6imalaali11@gmail.com>
2023-09-11 16:34:54 +0300

    fixing

--- django_react_proj/frontend/src/components/Header.js
+++ django_react_proj/frontend/src/components/Header.js
@@ -4,7 +4,7 @@ import LogOut from './Auth/LogOut';
 function Header({ user, setUser }) {
   return (
     <div className="text-center">
-      <h1>Gene Picks</h1>
+      <h1>Fix Fix</h1>
 
       {user ? <LogOut user={user} setUser={setUser} /> : null}
     </div>
