fc30121436614b69174b0d49a25cc85ef4308b48
jameela12 <jameela.khalil.95@gmail.com>
2023-09-12 11:31:20 +0300

    worked on the player box

--- django_react_proj/main_app/serializers.py
+++ django_react_proj/main_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Song
+from .models import User, Song, Album
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -13,3 +13,8 @@ class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = ('pk', 'title', 'artist', 'audio_file')
+
+class AlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Album
+        fields = ['name', 'total_songs', 'release_date', 'image_url']
